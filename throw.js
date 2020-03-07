bd178bd6f3b1a67769eeaf081b0b668ce3cebf73
JiYounKang <kingeve@gmail.com>
2020-03-07 15:23:55 +0100

    throw

---
+++ throw.js
@@ -0,0 +1,21 @@
+
+            function myFunction()
+            {
+               var winPoints = 3;
+               var drawPoints = 1;
+               
+               try{
+                  if ( drawPoints == 1 ){
+                     throw( "Real Madrid will not qualify." ); 
+                  }
+                  
+                  else
+                  {
+                     var qualification = winPoints + drawPoints;
+                  }
+               }
+               
+               catch ( e ) {
+                  alert("Error: " + e );
+               }
+            }
